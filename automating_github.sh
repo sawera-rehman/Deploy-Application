2759bcb8c31cdff0e29cab8af1978b6e8010c8e8
Ubuntu <ubuntu@ip-172-31-15-178.ap-southeast-2.compute.internal>
2023-12-01 19:53:13 +0000

    automating git push

---
+++ automating_github.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir task1
+cd task1
+touch task1.txt
+echo "Working on automating code push">task1.tx
+username = "sawera-rehman"
+token ="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"
+git add .
+git commit -m "working on automating code push"
+git push https://sawera-rehman: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@github.com/sawera-rehman/Deploy-Application.git main
---
+++ backend.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+mkdir task
+cd task
+touch classTask.txt
+echo "Working on the task">classTask.txt
+git add .
+git commit -m "worked on the task"
+git push origin main
